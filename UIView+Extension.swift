3cc4e0bf712b657dae59a0f4bf38b95f45246fdf
jungwook <wwwshe1@nate.com>
2019-11-07 17:26:16 +0900

    Shadow 기능 추가

---
+++ UIView+Extension.swift
@@ -0,0 +1,62 @@
+//
+//  UIView+Extension.swift
+//  CornerShadowSample
+//
+//  Created by jungwook on 2019/11/07.
+//  Copyright © 2019 jungwook. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addBorderShadow(color:UIColor, opacity:Float = 0.5, edge:AIEdge, shadowSize:CGFloat)    {
+        
+        var rect : CGRect = .zero
+        switch edge {
+        case .Top:
+            rect = CGRect(x: 0, y: -shadowSize, width: self.frame.width, height: shadowSize)
+        case .Left:
+            rect = CGRect(x: 0, y: 0, width: shadowSize, height: self.frame.height)
+        case .Bottom:
+            rect = CGRect(x: 0, y: self.frame.height, width: self.frame.width , height: shadowSize)
+        case .Right:
+            rect = CGRect(x: self.frame.width, y: 0, width: shadowSize , height: self.frame.height)
+            
+            
+        case .Top_Left:
+            rect = CGRect(x: -shadowSize, y: -shadowSize, width: self.frame.size.width - shadowSize, height: self.frame.size.height - shadowSize)
+        case .Top_Right:
+             rect = CGRect(x: shadowSize , y: -shadowSize , width: self.frame.size.width + shadowSize, height: self.frame.size.height)
+        case .Bottom_Left:
+             rect = CGRect(x: -shadowSize  , y: shadowSize, width: self.frame.size.width, height: self.frame.size.height + shadowSize)
+        case .Bottom_Right:
+            rect = CGRect(x: shadowSize , y: shadowSize, width: self.frame.size.width + shadowSize, height: self.frame.size.height + shadowSize)
+            
+            
+        case .All:
+            rect = CGRect(x: -shadowSize , y: -shadowSize, width: self.frame.size.width + (shadowSize * 2), height: self.frame.size.height + (shadowSize * 2))
+        }
+        
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+        self.layer.shadowOpacity = opacity
+        let shadowPath = UIBezierPath(rect: rect)
+        self.layer.shadowPath = shadowPath.cgPath
+        
+    }
+}
+
+enum AIEdge:Int {
+    case
+    Top,
+    Left,
+    Bottom,
+    Right,
+    Top_Left,
+    Top_Right,
+    Bottom_Left,
+    Bottom_Right,
+    All
+}
