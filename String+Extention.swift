07aa463224e37d896b4a732d5a1de09ab725b345
jungwook <wwwshe1@nate.com>
2019-11-12 14:45:20 +0900

    String extention add

---
+++ String+Extention.swift
@@ -0,0 +1,48 @@
+//
+//  String+Extention.swift
+//  TabGestureSample
+//
+//  Created by jungwook on 2019/11/12.
+//  Copyright © 2019 jungwook. All rights reserved.
+//
+
+import Foundation
+
+
+extension String {
+    subscript(value: Int) -> Character {
+        self[index(at: value)]
+    }
+}
+
+extension String {
+    subscript(value: NSRange) -> String {
+        self[value.lowerBound..<value.upperBound]
+    }
+}
+
+extension String {
+    subscript(value: Range<Int>) -> String {
+        String(self[index(at: value.lowerBound)..<index(at: value.upperBound)])
+    }
+    subscript(value: ClosedRange<Int>) -> String {
+        String(self[index(at: value.lowerBound)...index(at: value.upperBound)])
+    }
+    subscript(value: PartialRangeUpTo<Int>) -> String {
+        String(self[..<index(at: value.upperBound)])
+    }
+    
+    subscript(value: PartialRangeThrough<Int>) -> String {
+        String(self[...index(at: value.upperBound)])
+    }
+    
+    subscript(value: PartialRangeFrom<Int>) -> String {
+        String(self[index(at: value.lowerBound)...])
+    }
+}
+
+private extension String {
+    func index(at offset: Int) -> String.Index {
+        index(startIndex, offsetBy: offset)
+    }
+}
