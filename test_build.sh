245c026fee432ddac8e3c6e9a50bb84b431185cc
jungwook <wwwshe1@nate.com>
2019-11-18 13:07:01 +0900

    Test sh파일 수정

--- test_build.sh
+++ test_build.sh
@@ -1,1 +1,13 @@
-xcodebuild test -scheme sample -project sample.xcodeproj -destination "platform=iOS Simulator,name=iPhone X,OS=13.2.2"
+######################
+# Param setup
+######################
+TARGET_NAME=sample
+SCHEME=${TARGET_NAME}
+CONFIGURATION=Debug
+PROJECT_NAME=sample
+SRCROOT=.
+
+
+
+
+xcodebuild test -scheme ${SCHEME} -project ${PROJECT_NAME}.xcodeproj -destination "platform=iOS Simulator,name=iPhone X,OS=13.2.2"
