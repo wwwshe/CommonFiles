91563a41be575d3e89c8208cf913afa7c59a2087
jungwook <wwwshe1@nate.com>
2019-11-19 14:57:07 +0900

    UIView Extension 추가 및 수정

--- UIView+Extension.swift
+++ UIView+Extension.swift
@@ -62,15 +62,19 @@ enum AIEdge:Int {
 }
 
 
-
-
-extension UIView{
-    func tapActionKeyBoardHide(){
-        isUserInteractionEnabled = true
-        let tap =  UITapGestureRecognizer(target: self, action: #selector(keyBoardHide))
-        self.addGestureRecognizer(tap)
-    }
-    @objc func keyBoardHide(){
-        self.endEditing(true)
+extension UIView {
+    func screenshot() -> UIImage {
+        if #available(iOS 10.0, *) {
+            let format = UIGraphicsImageRendererFormat()
+            return UIGraphicsImageRenderer(size: bounds.size, format: format).image { _ in
+                drawHierarchy(in: CGRect(origin: .zero, size: bounds.size), afterScreenUpdates: true)
+            }
+        } else {
+            UIGraphicsBeginImageContextWithOptions(self.bounds.size, false, UIScreen.main.scale)
+            drawHierarchy(in: self.bounds, afterScreenUpdates: true)
+            let image = UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
+            UIGraphicsEndImageContext()
+            return image
+        }
     }
 }
