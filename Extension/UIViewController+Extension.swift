8174b5b61c4289a3d1aeee6583dc696ad26a0ab5
jungwook <wwwshe1@nate.com>
2020-01-02 16:26:32 +0900

    Toast추가

--- Extension/UIViewController+Extension.swift
+++ Extension/UIViewController+Extension.swift
@@ -15,9 +15,9 @@ extension UIViewController{
         let navBarColorImage = bgColor.image()
         navBar.setBackgroundImage(navBarColorImage, for: .default)
         navBar.tintColor = UIColor.white
-        navBar.titleTextAttributes = [.foregroundColor: titleColor]
+        navBar.titleTextAttributes = [.foregroundColor: titleColor, .font : UIFont.systemFont(ofSize: 24)]
+//        navBar.setTitleVerticalPositionAdjustment(3, for:.default)
     }
-    
 }
 
 extension UIViewController {
@@ -37,5 +37,32 @@ extension UIViewController {
                 return prev
             }
         }
+    }
+    var rootController : UIViewController?{
+        get{
+            return UIApplication.shared.delegate!.window??.rootViewController
+        }
+    }
+}
+
+
+extension UIViewController {
+    func showToast(message : String, font: UIFont) {
+        
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = font
+        toastLabel.textAlignment = .center;
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
     }
 }
