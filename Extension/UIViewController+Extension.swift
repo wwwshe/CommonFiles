e085c8f66afe9fac0a7837278d9a48431d14e4c0
jungwook <wwwshe1@nate.com>
2020-01-03 16:04:37 +0900

    패딩라벨 추가

--- Extension/UIViewController+Extension.swift
+++ Extension/UIViewController+Extension.swift
@@ -47,9 +47,9 @@ extension UIViewController {
 
 
 extension UIViewController {
-    func showToast(message : String, font: UIFont) {
-        
-        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+    func showToast(message : String, font: UIFont, width : CGFloat = 150) {
+        let toastLabel = PaddingLabel(10, 10, 10, 10)
+
         toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
         toastLabel.textColor = UIColor.white
         toastLabel.font = font
@@ -58,11 +58,18 @@ extension UIViewController {
         toastLabel.alpha = 1.0
         toastLabel.layer.cornerRadius = 10;
         toastLabel.clipsToBounds  =  true
+       
         self.view.addSubview(toastLabel)
+        toastLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            toastLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor,constant: 0),
+            toastLabel.bottomAnchor.constraint(equalTo: self.view.bottomAnchor,constant: -50),
+        ])
         UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
             toastLabel.alpha = 0.0
         }, completion: {(isCompleted) in
             toastLabel.removeFromSuperview()
         })
+        
     }
 }
