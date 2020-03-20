e61ca7bfd24e89e1d532f352ae2fd6b89fdc274b
jungwook <wwwshe1@nate.com>
2020-03-20 18:57:01 +0900

    RealmManager 수정

--- Utility/RealmManager.swift
+++ Utility/RealmManager.swift
@@ -12,107 +12,6 @@ import RealmSwift
 class RealmManager{
     static let shared = RealmManager()
     
-    let networking = NetworkManager.shared
-    
-    var isHaveLineData : Bool{
-        get{
-            let realm = try! Realm()
-            let obj = realm.objects(SubwayLineInfoElement.self)
-            if obj.isEmpty{
-                return false
-            }else{
-                return true
-            }
-        }
-    }
-    var isHaveStationData : Bool{
-        get{
-            let realm = try! Realm()
-            let obj = realm.objects(StationData.self)
-            if obj.isEmpty{
-                return false
-            }else{
-                return true
-            }
-        }
-    }
-    
-    @discardableResult 
-    func setLineDatas(idx : Int) -> SubwayLineInfo{
-        let realm = try! Realm()
-        let data = getLineDatas(idx: idx)
-        let lineInfos = decodeSubwayLineInfo(data: data)
-        let infos = lineInfos.sorted { (info1, info2) -> Bool in
-            return info1.stationNm < info2.stationNm
-        }
-        
-        try! realm.write {
-            realm.add(infos, update: true)
-        }
-        return lineInfos
-    }
-    
-    func getLineDatas(idx : Int) -> Data{
-        let url = "\(OPEN_API_BASE_URL)\(OPEN_API_LINE_URL)\(TYPE_JSON)\(SUBWAY_LINE_INFO)/1/100/ / /0\((idx))호선"
-        var returnData = Data()
-        networking.requestSync(url: url, method : .get) { (result) in
-            switch result{
-            case .success(let data):
-                returnData = data
-            case .failure(let error):
-                print(error)
-            }
-        }
-        return returnData
-    }
-    
-    func decodeSubwayLineInfo(data : Data) -> SubwayLineInfo{
-        var lineInfos = SubwayLineInfo()
-        
-        let decoder = JSONDecoder()
-        do {
-            let json = try JSONSerialization.jsonObject(with: data, options: [])
-            let dic = json as! [String : Any]
-            let topDic = dic["SearchSTNBySubwayLineInfo"] as! [String : Any]
-            let result = topDic["RESULT"] as! [String : Any]
-            let code = result["CODE"] as! String
-            
-            if code == OPEN_API_SUCCESE_CODE{
-                let rows = topDic["row"] as Any
-                let infoData = try JSONSerialization.data(withJSONObject: rows, options: .prettyPrinted)
-                var datas = try decoder.decode(SubwayLineInfo.self, from: infoData)
-                
-                datas = datas.map { (sub) -> SubwayLineInfoElement in
-                    var temp = sub
-                    temp.stationNm += "역"
-                    return temp
-                }
-                
-                
-                lineInfos = datas
-            }
-        } catch {
-            DLogPrint("error : \(error)")
-        }
-        return lineInfos
-    }
-    
-    func searchStation(name : String) -> SubwayLineInfo{
-        let realm = try! Realm()
-        let obj = realm.objects(SubwayLineInfoElement.self)
-        let predicate = NSPredicate(format: "stationNm contains[c] %@", name)
-        let filterResult = obj.filter(predicate)
-        if !filterResult.isEmpty{
-            var stations = Array(filterResult)
-            stations = stations.sorted { (left, right) -> Bool in
-                return left.stationNm < right.stationNm
-            }
-            return stations
-        }else{
-            return SubwayLineInfo()
-        }
-    }
-    
     
     func setStationDatas(datas : StationDatas){
         let realm = try! Realm()
@@ -129,27 +28,9 @@ class RealmManager{
 
         if !filterResult.isEmpty{
             let stations = Array(filterResult)
-            
             return stations[0]
         }else{
             return StationData()
-        }
-    }
-    func selectStationLines(name : String) -> [String]{
-        var array = [String]()
-        let realm = try! Realm()
-        let obj = realm.objects(StationData.self)
-        let predicate = NSPredicate(format: "stationNm=%@", name)
-        let filterResult = obj.filter(predicate)
-        
-        if !filterResult.isEmpty{
-            let stations = Array(filterResult)
-            for station in stations{
-                array.append(station.lineNum)
-            }
-            return array
-        }else{
-            return array
         }
     }
 }
